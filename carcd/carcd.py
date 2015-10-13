c7f506ac3ac4b3176d1e7af7e7084cdade81c227
jeroyang <jeroyang@gmail.com>
2015-10-13 11:18:00 +0800

    title and filename related functions

--- carcd/carcd.py
+++ carcd/carcd.py
@@ -4,3 +4,117 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
+
+import constants
+import pinyin
+import string
+import re
+from collections import OrderedDict
+import sys
+
+def beautify(context):
+    """
+    Keep better looking in ascii-chinese combined context
+    add space between ascii_letters and Chinese
+    avoid multiple spaces, 
+    avoid space between chinese and symbols,
+
+    """
+    if sys.hexversion >= 0x03000000:
+        chinese = r'(?a)[^{}]'.format(re.escape(string.printable))
+    else: 
+        chinese = r'[^{}]'.format(re.escape(string.printable))
+    symbols = r'[{}]'.format(re.escape(string.punctuation))
+    ascii = r'\w'
+    context = re.sub(r'({0})({1})'.format(chinese, ascii), r'\1 \2', context)
+    context = re.sub(r'({1})({0})'.format(chinese, ascii), r'\1 \2', context)
+    context = re.sub(r'({0}) ({1})'.format(chinese, symbols), r'\1\2', context)
+    context = re.sub(r'({1}) ({0})'.format(chinese, symbols), r'\1\2', context)
+    remove_multispace = re.sub(r'\s+', ' ', context)
+    return remove_multispace
+
+def asciilize(context):
+    """
+    Transform Chinese characters to pinyin, 
+    multibyte symbol to unibyte symbol
+    """
+    output = []
+    for ch in context:
+        if ch in string.printable:
+            output.append(ch)
+        elif ch in constants.FULL2HALF:
+            output.append(constants.FULL2HALF.get(ch, ''))
+        else:
+            output.append(' ')
+            output.extend(pinyin.get(ch).capitalize())
+    return ''.join(output).replace('  ', ' ')
+
+def name_split(name):
+    """
+    Split a cd filename to multiple parts
+    return as a OrderedDict:
+    {
+        <category>: <string>, 
+    }
+    the categories are following:
+    `number` for leading numbers
+    `title` for title name
+    `ext` for file extension
+    'space' for space ' '
+    """
+    categories = ['number', 'space', 'title', 'ext']
+    pattern = (r'(?P<number>\d+(-?\d+))'
+                '(?P<space> )?'
+                '(?P<title>.*?)'
+                '(?P<ext>\....)')
+
+    itemdict = re.match(pattern, name).groupdict()
+    itemlist = [(category, itemdict[category]) for category in categories]
+    return OrderedDict(itemlist)
+
+def name_join(items):
+    """
+    Join nameitems, eleminate None in the OrderedDict
+    """
+    return ''.join([item for item in items.values() if item is not None])
+
+def number_format(number_string, fill=2):
+    """
+    add padding zeros to make alinged numbers
+    ex. 
+
+    >>> number_format('2')
+    '02'
+
+    >>> number_format('1-2')
+    '01-02'
+    """
+    output = []
+    digits_spliter = r'(?P<digit>\d+)|(?P<nondigit>.)'
+    for token in [m.groups() for m in re.finditer(digits_spliter, number_string)]:
+        if token[0] is None:
+            output.append(token[1])
+        else:
+            output.append(token[0].zfill(2))
+    return ''.join(output)
+
+def name_handle(name):
+    """
+    Complicated processes to manipulate a given filename
+    """
+    items = name_split(name)
+    output = []
+    for item in items.items():
+        if (item[0] == 'number') & (item[1] is not None):
+            output.append(('number', number_format(item[1])))
+        if (item[0] == 'space') & (item[1] is not None):
+            output.append(('space', ' '))
+        if (item[0] == 'title') & len(set(item[1]) - set(string.printable)) > 0:
+            output.append(('title', asciilize(item[1]) + item[1]))
+        if (item[0] == 'ext') & (item[1] is not None):
+            output.append(('ext', item[1]))
+    items = OrderedDict(output)
+    return beautify(name_join(items))
+
+
+
--- tests/test_carcd.py
+++ tests/test_carcd.py
@@ -13,6 +13,7 @@ Tests for `carcd` module.
 """
 
 import unittest
+from collections import OrderedDict
 
 from carcd import carcd
 
@@ -22,8 +23,49 @@ class TestCarcd(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_beautify(self):
+        data = '我是1個  大呆 !!'
+        result = carcd.beautify(data)
+        wanted = '我是 1 個 大呆!!'
+
+    def test_asciilize(self):
+        result = carcd.asciilize('12 34 何大一！')
+        wanted = '12 34 He Da Yi!'
+        self.assertEqual(result, wanted)
+
+    def test_name_split(self):
+        name = '12-2 ABC 大大一.mp3'
+        result = carcd.name_split(name)
+        wanted = OrderedDict([
+                    ('number', '12-2'),
+                    ('space', ' '),
+                    ('title', 'ABC 大大一'),
+                    ('ext', '.mp3')
+                 ])
+        self.assertEqual(result, wanted)
+
+    def test_name_join(self):
+        items = [
+                    ('12-2', 'number'),
+                    (None, 'space'),
+                    ('ABC 大大一', 'title'),
+                    ('.mp3', 'ext')
+                 ]
+        wanted = '12-2ABC 大大一.mp3'
+
+    def test_number_format(self):
+        result = carcd.number_format('2', fill=2)
+        wanted = '02'
+        self.assertEqual(result, wanted)
+        result = carcd.number_format('2-2', fill=2)
+        wanted = '02-02'
+        self.assertEqual(result, wanted)
+
+    def test_name_handle(self):
+        name = '12-2 何大一.mp3'
+        result = carcd.name_handle(name)
+        wanted = '12-02 He Da Yi 何大一.mp3'
+        self.assertEqual(result, wanted)
 
     def tearDown(self):
         pass
